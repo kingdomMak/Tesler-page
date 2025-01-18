200610e457e08d7e446f240540f5704b9d2ced27
Olenikatshedza <makunamishatshedza@gmail.com>
2025-01-18 23:18:41 +0200

    Initial commit of Tesla landing page project

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('sign-in').addEventListener('click', function() {
+    alert('Sign In button clicked!'); // Placeholder for sign-in functionality
+    // You can redirect to a sign-in page or implement further functionality here
+});
