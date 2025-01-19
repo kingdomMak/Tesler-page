2f2d4198534567a1deb2a92e2c70c40755f80c12
Olenikatshedza <makunamishatshedza@gmail.com>
2025-01-19 20:13:03 +0200

    Re-uploading all files including styles to the repository

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('sign-in').addEventListener('click', function() {
-    alert('Sign In button clicked!'); // Placeholder for sign-in functionality
-    // You can redirect to a sign-in page or implement further functionality here
-});
